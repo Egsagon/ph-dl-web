5e4dacbd33ccadc34a344cadfe1282073b9179b4
@Egsagon <egsagon12@gmail.com>
2023-06-05 13:52:43 +0200

    logs correction + globals error

--- downloader.py
+++ downloader.py
@@ -25,14 +25,14 @@ def log(title: str, *text) -> None:
     # Attribute color to title
     color = COLORS[ hash(title) % len(COLORS) ]
     
-    print(f'\033[{color}m{title.upper()}\033[{color - 10}m', *text, '\033[0m')
+    print(f'\033[{color}m{title.upper()}\033[0m\033[{color - 10}m', *text, '\033[0m')
 
 def err(title: str, *text) -> None:
     '''
     Log an error.
     '''
     
-    print(f'\033[101m{title.upper()}\033[91m', *text, '\033[0m')
+    print(f'\033[101m{title.upper()}\033[0m\033[91m', *text, '\033[0m')
 
 
 @dataclass
@@ -109,6 +109,8 @@ class Worker:
             
             def receiver(cur: int, total: int) -> None:
                 # Handle updating the call data
+                
+                global partials
                 
                 # Update call status
                 call.progress = cur
